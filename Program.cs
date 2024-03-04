3636af82de557ed423cf525147d0a6b529324859
Matthew Cavallo <macavall@microsoft.com>
2024-03-04 14:30:40 -0600

    first

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebJobV3
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var builder = new HostBuilder();
+            builder.UseEnvironment(EnvironmentName.Development);
+            builder.ConfigureLogging((context, b) =>
+            {
+                b.AddConsole();
+            });
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddAzureStorageQueues();
+            });
+            var host = builder.Build();
+            using (host)
+            {
+                await host.RunAsync();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# WebJobV3
